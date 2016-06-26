acdd02f4d37d7189f571b19635fc696b5d2fcd0b
Ryu Hanaoka <doradora56@gmail.com>
2016-06-27 01:12:32 +0900

    4.2.1 sprinkle p93

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "math/rand"
+    "time"
+    "bufio"
+    "os"
+    "fmt"
+    "strings"
+)
+
+const otherWord = "*"
+var transforms = []string{
+    otherWord,
+    otherWord,
+    otherWord,
+    otherWord,
+    otherWord + "app",
+    otherWord + "site",
+    otherWord + "time",
+    "get" + otherWord,
+    "go" + otherWord,
+    "lets" + otherWord,
+}
+
+func main() {
+    rand.Seed(time.Now().UTC().UnixNano())
+    s := bufio.NewScanner(os.Stdin)
+    for s.Scan() {
+        t := transforms[rand.Intn(len(transforms))]
+        fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
+    }
+}
